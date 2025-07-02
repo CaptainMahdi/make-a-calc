8fa57c55a373ad6b75b520639962d2d53d836119
CaptainMahdi <mahdi.hteit@gmail.com>
2025-07-01 21:00:58 -0500

    added operations and print result

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("this is the calc")
+print("Welcome to the calculator")
+
+num1 = float(input("Please provide the first number: "))
+num2 = float(input("Please provide the second number: "))
+operation = input("What operation would you like to use? addition(+), subtraction(-), multiplication(*), division(/): ")
+
+if operation == "+": 
+    result = num1 + num2
+elif operation == "-":
+    result = num1 - num2
+elif operation == "*":
+    result = num1 * num2
+elif operation == "/":
+    result = num1 / num2
+else:
+    print("Invalid operation")
+
+
+print("The answer is: ", result)
